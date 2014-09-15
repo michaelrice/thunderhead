f556972aea706e1c28d5153de101376c676f93f7
Michael Rice <michael.rice@rackspace.com>
2014-09-14 22:46:50 -0500

    removed lxml dep and switched to xml.etree from base python

--- thunderhead/parser/customers.py
+++ thunderhead/parser/customers.py
@@ -12,7 +12,7 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from lxml import etree
+from xml.etree import ElementTree as etree
 
 
 class Customer(object):
