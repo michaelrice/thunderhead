81405e3e7cc02f512e91ba31adc65af269b7eb70
Michael Rice <michael.rice@rackspace.com>
2014-09-17 20:10:37 -0500

    fixing failing build on py3

--- thunderhead/builder/rules.py
+++ thunderhead/builder/rules.py
@@ -11,3 +11,37 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import SubElement
+from xml.etree.ElementTree import tostring
+
+import requests
+
+from thunderhead.parser import rules
+
+
+def get_all_rules(connection):
+    connection.command_path = "rules"
+    extra_headers = {connection.header_key: connection.token}
+    url = connection.build_url()
+    verify_ssl = connection.verify_ssl
+    res = requests.get(url=url, headers=extra_headers, verify=verify_ssl)
+    if res.status_code > 210:
+        return
+    body = res.content
+    return rules.parse_all_rules(body)
+
+
+def get_rule(connection, rule_id):
+    connection.command_path = "rule/{0}".format(rule_id)
+    extra_headers = {connection.header_key: connection.token}
+    url = connection.build_url()
+    verify_ssl = connection.verify_ssl
+    res = requests.get(url=url, headers=extra_headers, verify=verify_ssl)
+    if res.status_code > 210:
+        return
+    body = res.content
+    return rules.parse_rule(body)
+
+
--- thunderhead/parser/rules.py
+++ thunderhead/parser/rules.py
@@ -11,3 +11,36 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
+from xml.etree import ElementTree as etree
+
+
+def parse_all_rules(body):
+    if body is None:
+        return None
+    rules = etree.fromstring(body)
+    if len(rules) == 0:
+        return None
+    rule_list = []
+    for rule in rules.getchildren():
+        rule_dict = _parse_rule(rule)
+        rule_list.append(rule_dict)
+    print rule_list
+    return rule_list
+
+
+def parse_rule(body):
+    if body is None:
+        return None
+    rule = etree.fromstring(body)
+    return _parse_rule(rule)
+
+
+def _parse_rule(rule):
+    v = {}
+    for rule_info in rule.getchildren():
+        # this is messy but it seems cleaner than dealing with the namespace
+        tag = rule_info.tag.split("}")[1][0:]
+        text = rule_info.text
+        v[tag] = text
+    return v
